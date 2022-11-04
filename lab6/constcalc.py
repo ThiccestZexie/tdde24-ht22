1229127dcb07930d39c4f7b87622e93434ab5967
andru321 <andru321@su04-114.ad.liu.se>
2022-11-04 13:21:12 +0100

    Added basic functions, still empty

--- lab6/constcalc.py
+++ lab6/constcalc.py
@@ -2,18 +2,40 @@ from calc import *
 
 def exec_program(program):
     if is_program(program): # Checks if the input is a valid program
-        p = program_statements(program)[0]
+        p = program_statements(program)
+        return exec_statements(p)
     else:
         print("this isnt a calc program")
         return False
 
+def exec_statements(p):
+    for statement in p:
+        if not exec_statement(statement):
+            return False
 
-    if is_statements(p):
-        for statement in p:
-            if is_binaryexpr(statement):
-                return 
+def exec_statement(p):
+    1
 
+def exec_assignement(p):
+    1
 
+def exec_repetition(p):
+    1
+
+def exec_selection(p):
+    1
+
+def exec_input(p):
+    1
+
+def exec_output(p):
+    1
+
+def exec_binaryexpr(p):
+    1
+
+def exec_condition(p):
+    1
 
 
 
