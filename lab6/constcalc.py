bd621d73adbc4b32576ebfd7c2eeadf362624101
andru321 <andru321@su04-114.ad.liu.se>
2022-11-04 15:01:47 +0100

    fixed calc1

--- lab6/constcalc.py
+++ lab6/constcalc.py
@@ -8,65 +8,86 @@ def exec_program(program):
         print("this isnt a calc program")
         return False
 
+
 def exec_statements(p):
 
     if not empty_statements(p):
         
-        exec_statement(first_statement(p))
+        pathfind(first_statement(p))
 
-        exec_statements(rest_statements(p))
+        pathfind(rest_statements(p))
+
+
+def pathfind(p):
+    
+    if is_statement(p):
+        return exec_statement(p)
+    
+    else:
+        return exec_expression(p)
 
 
 def exec_statement(p):
 
-    if is_statement(p):
+    if is_assignment(p):
+        exec_assignement(p)
 
-        if is_assignment(p):
-            exec_assignement(p)
+    elif is_repetition(p):
+        exec_repetition(p)
 
-        elif is_repetition(p):
-            exec_repetition(p)
+    elif is_selection(p):
+        exec_selection(p)
 
-        elif is_selection(p):
-            exec_selection(p)
+    elif is_input(p):
+        exec_input(p)
 
-        elif is_input(p):
-            exec_input(p)
+    elif is_output(p):
+        exec_output(p)
 
-        elif is_output(p):
-            exec_output(p)
 
-        elif is_binaryexpr(p):
-            exec_binaryexpr(p)
+def exec_expression(p):
+    
+    if is_variable(p):
+        return p
 
-        elif is_condition(p):
-            exec_condition(p)
+    elif is_constant(p):
+        return p
+    
+    elif is_binaryexpr(p):
+        return exec_binaryexpr(p)
+
+    elif is_condition(p):
+        return exec_condition(p)
 
 
 def exec_assignement(p):
     1
 
+
 def exec_repetition(p):
     if repetition_condition(p):
-        exec_statement(repetition_statements(p))
+        pathfind(repetition_statements(p))
         exec_repetition(p)
     else:
-        exec_statement(p)
+        pathfind(p)
 
 
 def exec_selection(p):
 
-    if exec_statement(selection_condition(p)):
-        return exec_statement(selection_true_branch(p))
+    if pathfind(selection_condition(p)):
+        return pathfind(selection_true_branch(p))
 
     elif selection_has_false_branch(p):
-        return exec_statement(selection_false_branch(p))
+        return pathfind(selection_false_branch(p))
+
 
 def exec_input(p):
-    1
+    return input_variable(p)
+
 
 def exec_output(p):
-    print(output_expression(p))
+    print(pathfind(output_expression(p)))
+
 
 def exec_binaryexpr(p):
 
@@ -104,6 +125,7 @@ def exec_condition(p):
 
 
     
-calc1 = ['calc', ['if', [3, '>', 5], ['print', 2], ['print', 4]]]
+calc1 = ['calc', ['if', [3, '<', 5], ['print', 2], ['print', 4]]]
 calc2 = ['calc', ['while', [3, '<', 5], ['print', 2]], ['print', 'end']]
+calc3 = ['calc', ['print', [3, '+', 5]]]
 exec_program(calc1)
