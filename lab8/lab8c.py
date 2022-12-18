5499018d5b7aaf06bf07139e97e002883d06d71d
danal315 <danal315@student.liu.se>
2022-12-19 00:06:22 +0100

    börjat med 8d

--- lab8/lab8c.py
+++ lab8/lab8c.py
@@ -6,18 +6,23 @@ def remove(cal_name: str, day: int, month: str, start: str) -> None:
     day = new_day(day)
     mon = new_month(month)
     start = new_time_from_string(start)
-    cal_year = get_calendar(cal_name)
-    cal_month = cy_get_month(mon, cal_year)
-    cal_day = cm_get_day(cal_month, day)
+    cal_year = get_calendar(cal_name) #gets the year
+    cal_month = cy_get_month(mon, cal_year) # gets the month
+    cal_day = cm_get_day(cal_month, day) # get the date 
+    new_cal_day = []
+    for app in cal_day.appointments:
+        new_cal_day.append(cd_plus_appointment(cal_day, app))
+    new_cal_month = cm_plus_cd(cal_month, new_cal_day)
+    new_cal_year = cy_plus_cm(cal_year, new_cal_month)
 
-    for appointment in cal_day.appointments:
+    for appointment in new_cal_day.appointments:  # checks if the start time exists in the date
         if start == ts_start(app_span(appointment)):
-            cal_day.appointments.remove(appointment)
-
-
-
+            new_cal_day.appointments.remove(appointment)
+    return new_cal_year
+ 
 
 def test_labc():
+    create("Jayne")
     create("Jayne")
     book("Jayne", 20, "sep", "12:00", "14:00", "Rob train")
     book("Jayne", 20, "sep", "15:00", "16:00", "Escape with loot")
