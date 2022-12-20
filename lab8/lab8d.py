04c4eb7fb971cae8a31f49b0db5f254cdc405b28
andru321 <andru321@su04-116.ad.liu.se>
2022-12-20 14:43:11 +0100

    Finished and demonstrated lab8d

--- lab8/lab8d.py
+++ lab8/lab8d.py
@@ -18,6 +18,8 @@ else:
     from lab8b import *
 
 def show_free(cal_name, day, month, start: Time, end: Time) -> None:
+    """ Prints the free time in a given day using other functions """
+
     start = new_time_from_string(start)
     end = new_time_from_string(end)
     day = new_day(day)
@@ -26,42 +28,55 @@ def show_free(cal_name, day, month, start: Time, end: Time) -> None:
     cal_month = cy_get_month(mon, cal_year) # gets the month
     cal_day = cm_get_day(cal_month, day) # get the date 
 
-
     free_tss = free_spans(cal_day, start, end)
     show_time_spans(free_tss)
 
-def free_spans(cal_day: CalendarDay, start: Time, end: Time) -> TimeSpanSeq:
 
-    if start == end:
-        print('start and end are the same')
+def free_spans(cal_day: CalendarDay, start: Time, end: Time) -> TimeSpanSeq:
+    """ Returns a TimeSpanSeq of the free time in a given cal_day between start and end """
+
+    if time_equals(end, start):
+        print('start and end time is the same, there is no time to print')
+        return new_time_span_seq()
+
+    elif not cal_day.appointments:
+        return new_time_span_seq([new_time_span(start,end)])
 
     else: # check for the first appointment of the day see start and go from "start" to that start and repeat at the end
         ts_list = [app_span(app) for app in cd_iter_appointments(cal_day)]
         ts_list = refine_list(ts_list, start, end)
-        return check_free(start, end, ts_list)
+        
+        return free_tss(start, end, ts_list)
 
 
-
-def check_free(start, end, ts_list, tss = None):
-    if ts_list == []:
-        tss = tss_plus_span(tss, new_time_span(start, end))
-        return tss
+def free_tss(start: Time, end: Time, ts_list: List[TimeSpan], tss: TimeSpanSeq = None) -> TimeSpanSeq:
+    """ Creates a TimeSpanSeq with all the free time between start and end """
 
     if tss == None:
         tss = new_time_span_seq()
 
+    if ts_list == []:
+        if not time_equals(start, end):
+            tss = tss_plus_span(tss, new_time_span(start, end))
+        return tss
+
     if time_precedes(start, ts_start(ts_list[0])):
         tss = tss_plus_span(tss, new_time_span(start, ts_start(ts_list[0])))
 
-    return check_free(ts_end(ts_list[0]), end, ts_list[1:], tss)
+    return free_tss(ts_end(ts_list[0]), end, ts_list[1:], tss)
 
 
-def refine_list(ts_list, start, end):
-    #TODO fixa så den lägger in de korrekta tiderna
+def refine_list(ts_list: List[TimeSpan], start: Time, end: Time) -> List[TimeSpan]:
+    """ Creates a new list from the given ts_list so that all the times are within the start and end """
+
     new_ts_list = []
+
     for ts in ts_list:
         if time_precedes_or_equals(start, ts_start(ts)) and time_precedes_or_equals(ts_end(ts), end):
             new_ts_list.append(ts)
+
+        elif time_precedes(ts_start(ts), start) and time_precedes(end, ts_end(ts)):
+            new_ts_list.append(new_time_span(start, end))
 
         elif time_precedes(ts_start(ts), start) and time_precedes(start, ts_end(ts)):
             new_ts_list.append(new_time_span(start, ts_end(ts)))
@@ -73,10 +88,7 @@ def refine_list(ts_list, start, end):
 
 
 if __name__ == "__main__":
-    create("Jayne")
-    book("Jayne", 20, "sep", "15:00", "16:00", "Escape with loot")
-    book("Jayne", 20, "sep", "10:00", "11:00", "Escape with loot")
-    book("Jayne", 20, "sep", "18:00", "20:00", "Escape with loot")
-    book("Jayne", 20, "sep", "07:00", "09:00", "Escape with loot")
-
-    show_free("Jayne", 20, "sep", "08:00", "19:00")
+    create("Andreas")
+    book("Andreas", 26, "sep", "08:00", "15:00", "School")
+    book("Andreas", 26, "sep", "17:00", "21:00", "Birthday party")
+    show_free("Andreas", 26, "sep", "00:00", "23:00")
--- lab8/lab8d_tests.py
+++ lab8/lab8d_tests.py
@@ -10,7 +10,7 @@ def create_tests_for_free_span() -> dict:
     test_cases = dict()
 
     store_test_case(
-        test_cases,
+        test_cases, #Premade test
         1,
         start_str="08:00",  # Search interval starts
         end_str="21:00",  # Search interval ends
@@ -20,6 +20,61 @@ def create_tests_for_free_span() -> dict:
 
     # -------- YOUR TEST CASES GO HERE -----------------------
     # For each case, add a brief description of what you want to test.
+
+    store_test_case(
+        test_cases, #Tests bookings over the start and end times
+        2,
+        start_str="08:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=["07:00-09:00", "10:00-11:00", "15:00-16:00", "18:00-20:00"],  # This day's appointments
+        exp_result=["09:00-10:00", "11:00-15:00", "16:00-18:00"],
+    )  # Expected free time
+
+    store_test_case(
+        test_cases, #Tests empty booking
+        3,
+        start_str="08:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=[],  # This day's appointments
+        exp_result=["08:00-19:00"],
+    )  # Expected free time
+
+    store_test_case(
+        test_cases, #Tests fully booked
+        4,
+        start_str="08:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=["08:00-19:00"],  # This day's appointments
+        exp_result=[],
+    )  # Expected free time
+
+    store_test_case(
+        test_cases, #Tests booking before the start time
+        5,
+        start_str="11:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=["08:00-10:00"],  # This day's appointments
+        exp_result=["11:00-19:00"],
+    )  # Expected free time
+
+    store_test_case(
+        test_cases, #Tests connected booking
+        6,
+        start_str="08:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=["08:00-10:00", "10:00-12:00"],  # This day's appointments
+        exp_result=["12:00-19:00"],
+    )  # Expected free time
+
+    store_test_case(
+        test_cases, #Tests booking from before start time to after the end time
+        7,
+        start_str="08:00",  # Search interval starts
+        end_str="19:00",  # Search interval ends
+        booking_data=["06:00-21:00"],  # This day's appointments
+        exp_result=[],
+    )  # Expected free time
+
 
     print("Test cases generated.")
 
